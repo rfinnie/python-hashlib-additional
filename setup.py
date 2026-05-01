8c3ccbc0096ce62bc5840c7c0aed7005292c1576
Ryan Finnie <ryan@finnie.org>
2026-05-01 10:49:25 -0700

    Bump requires-python; add explicit build-system; add stub setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+# SPDX-PackageName: python-hashlib-additional
+# SPDX-PackageSupplier: Ryan Finnie <ryan@finnie.org>
+# SPDX-PackageDownloadLocation: https://codeberg.org/rfinnie/python-hashlib-additional
+# SPDX-FileCopyrightText: © 2026 Ryan Finnie <ryan@finnie.org>
+# SPDX-License-Identifier: MIT
+
+from setuptools import setup
+
+setup()
